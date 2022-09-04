b0adc979e6642ed5d6bbc9d0197c173383f12c73
roabhi <teskostudio@gmail.com>
2022-09-04 20:36:48 +0200

    first test on constructor

---
+++ test/unit/FundMe.test.js
@@ -0,0 +1,30 @@
+const { assert } = require("chai")
+const { deployments, ethers, getNamedAccounts } = require("hardhat")
+
+describe("FundMe", async function () {
+    let fundMe
+    let deployer
+    let mockV3Aggregator
+    beforeEach(async function () {
+        //deploy our fundMe contract
+        // using Hardhat-deploy
+
+        //We can also get accounts doing:
+        //const accounts = await ethers.getSigners()
+        //const accountZero = accounts[0]
+        deployer = (await getNamedAccounts()).deployer
+        await deployments.fixture(["all"])
+        fundMe = await ethers.getContract("FundMe")
+        mockV3Aggregator = await ethers.getContract(
+            "MockV3Aggregator",
+            deployer
+        )
+    })
+
+    describe("constructor", async function () {
+        it("sets the aggregator addresses correctly", async function () {
+            const response = await fundMe.priceFeed()
+            assert.equal(response, mockV3Aggregator.address)
+        })
+    })
+})
