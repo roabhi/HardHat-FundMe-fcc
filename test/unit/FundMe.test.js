3e02cb18c4a912a0cf9224d7f636ab60cbebd202
roabhi <teskostudio@gmail.com>
2022-09-06 17:58:12 +0200

    implemented test for fund and withdraw functions

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,10 +1,13 @@
-const { assert } = require("chai")
+const { assert, expect, Assertion } = require("chai")
+const { FixedNumber } = require("ethers")
 const { deployments, ethers, getNamedAccounts } = require("hardhat")
 
 describe("FundMe", async function () {
     let fundMe
     let deployer
     let mockV3Aggregator
+    // const sendValue = "1000000000000000000" // 1 EHT
+    const sendValue = ethers.utils.parseEther("1")
     beforeEach(async function () {
         //deploy our fundMe contract
         // using Hardhat-deploy
@@ -25,6 +28,64 @@ describe("FundMe", async function () {
         it("sets the aggregator addresses correctly", async function () {
             const response = await fundMe.priceFeed()
             assert.equal(response, mockV3Aggregator.address)
+        })
+    })
+
+    describe("fund", async function () {
+        it("Fails if you donot send enough ETH", async function () {
+            //Way to check an error and pass the test anyways. Use expect().to.be[]
+            //Somehow this above use waffle ?
+            await expect(fundMe.fund()).to.be.revertedWith(
+                "You need to spend more ETH!"
+            )
+        })
+        it("Updates the amount funded data structure", async function () {
+            await fundMe.fund({ value: sendValue })
+            //npx hardhat test --grep "amount funded"
+            // to get only the this test i.e
+            const response = await fundMe.addressToAmountFunded(deployer)
+            assert.equal(response.toString(), sendValue.toString())
+        })
+        it("Adds funder to array of funders", async function () {
+            await fundMe.fund({ value: sendValue })
+            const funder = await fundMe.funders(0)
+            assert.equal(funder, deployer)
+        })
+    })
+
+    describe("withdraw", async function () {
+        beforeEach(async function () {
+            await fundMe.fund({ value: sendValue })
+        })
+        it("withdraw ETH from a single founder", async function () {
+            //Arrange
+            const startingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const startingDeloyerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+            //Act
+            const transactionResponse = await fundMe.withdraw()
+            const transactionRecepit = await transactionResponse.wait(1)
+            // gasCost
+            const { gasUsed, effectiveGasPrice } = transactionRecepit
+            const gasCost = gasUsed.mul(effectiveGasPrice) //mul = *
+
+            const endingFundingBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const endingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+
+            // Assert
+            assert.equal(endingFundingBalance, 0)
+            assert.equal(
+                startingFundMeBalance.add(startingDeloyerBalance),
+                endingDeployerBalance.add(gasCost).toString() //add = +
+                //Last line take into account gas transaction cost in order to assert equal amount on both sides
+            )
         })
     })
 })
