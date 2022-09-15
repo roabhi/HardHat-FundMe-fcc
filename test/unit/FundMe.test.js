dfc3fb46ee31eb2e6c1d5c3bdc4002612f8bbb92
roabhi <teskostudio@gmail.com>
2022-09-15 17:54:00 +0200

    added unit testing for withdrawing with multiple funders

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -87,5 +87,68 @@ describe("FundMe", async function () {
                 //Last line take into account gas transaction cost in order to assert equal amount on both sides
             )
         })
+        it("allows us to withdraw with multiple funders", async function () {
+            //Arrange
+            const accounts = await ethers.getSigners()
+
+            for (let i = 1; i < 6; i++) {
+                const fundMeConnectedContract = await fundMe.connect(
+                    accounts[i]
+                )
+                await fundMeConnectedContract.fund({ value: sendValue })
+            }
+            const startingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const startingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+
+            // console.log(
+            //     "startingDeployerBalance: " +
+            //         startingDeployerBalance / 100000000000000000000
+            // )
+
+            // Act:
+            const transactionResponse = await fundMe.withdraw()
+            const transactionReceipt = await transactionResponse.wait(1)
+            const { gasUsed, effectiveGasPrice } = transactionReceipt
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+
+            //Assert
+            const endingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const endingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+            // console.log(
+            //     "Final Deployer Balance: " +
+            //         startingFundMeBalance
+            //             .add(startingDeployerBalance)
+            //             .toString() /
+            //             100000000000000000000
+            // )
+            // console.log(
+            //     "endingDeployerBalance: " +
+            //         endingDeployerBalance.add(gasCost).toString()
+            // )
+            //Assert
+            assert.equal(endingFundMeBalance, 0)
+            assert.equal(
+                startingFundMeBalance.add(startingDeployerBalance).toString(),
+                endingDeployerBalance.add(gasCost).toString()
+            )
+
+            //Make sure that funders are reset properly:
+            await expect(fundMe.funders(0)).to.be.reverted
+
+            for (i = 1; i < 6; i++) {
+                assert.equal(
+                    await fundMe.addressToAmountFunded(accounts[i].address),
+                    0
+                )
+            }
+        })
     })
 })
